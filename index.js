736619656cd699a6fcb1a8a96a28a89f58bbe773
luchos-git <trevissan.luciano@gmail.com>
2026-09-14 20:27:07 -0300

    pepe

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-const express = require('express');
+// const express = require('express');
+import express from 'express'
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send(`Hello, World!`);
+});
+app.get('/personas', (req, res) => {
+  res.send(`listafo de personas`);
+});
+app.get('/personas/:id', (req, res) => {
+  const { id } = req.params;
+  res.send(`persona con id: ${id}`);
+});
+app.post('/personas', (req, res) => {
+  res.send(`crear persona`);
+});
+
+
+app.put('/personas', (req, res) => {
+  res.send(`actualizar persona`);
+});
+app.delete('/personas', (req, res) => {
+  res.send(`eliminar persona`);
 });
 
 app.listen(port, () => {
